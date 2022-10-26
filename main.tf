d1879c31687eb45638996a70ade6166762cf4067
Steven Koeven <skoeven@pm.me>
2022-10-26 16:24:01 -0600

    Add back missing instance

--- main.tf
+++ main.tf
@@ -14,6 +14,16 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
